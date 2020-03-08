95ca7f94ddba0945be01f27fef9b7c9e6da31885
George Tsifrikas <tsifrikas@workable.com>
2020-03-08 11:02:00 +0200

    Initial Commit

---
+++ COVID-19 Track/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  COVID-19 Track
+//
+//  Created by George Tsifrikas on 8/3/20.
+//  Copyright © 2020 George Tsifrikas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ COVID-19 TrackTests/COVID_19_TrackTests.swift
@@ -0,0 +1,34 @@
+//
+//  COVID_19_TrackTests.swift
+//  COVID-19 TrackTests
+//
+//  Created by George Tsifrikas on 8/3/20.
+//  Copyright © 2020 George Tsifrikas. All rights reserved.
+//
+
+import XCTest
+@testable import COVID_19_Track
+
+class COVID_19_TrackTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
